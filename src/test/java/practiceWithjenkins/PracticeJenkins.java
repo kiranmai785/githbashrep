4c2261eafda37b176b2a79b626543acaa40cad78
kiranmai785 <kiranmaisai8978@gmail.com>
2024-08-08 15:46:15 +0530

    1st commit

---
+++ src/test/java/practiceWithjenkins/PracticeJenkins.java
@@ -0,0 +1,11 @@
+package practiceWithjenkins;
+
+public class PracticeJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hii world");
+
+	}
+
+}
